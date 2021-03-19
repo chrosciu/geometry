4b06a1531ebf108b153ac28359f8d89d047fcf98
Marcin Chrost <marcin.chrost@contractors.roche.com>
2021-03-19 17:11:43 +0100

    section equals + hashcode + tests

--- src/main/java/com/chrosciu/geometry/model/Section.java
+++ src/main/java/com/chrosciu/geometry/model/Section.java
@@ -3,12 +3,14 @@ package com.chrosciu.geometry.model;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NonNull;
+import lombok.ToString;
 
 import java.util.Arrays;
 import java.util.List;
 
 @AllArgsConstructor
 @Getter
+@ToString
 public final class Section extends Polygon {
     @NonNull
     final Point start;
@@ -33,5 +35,21 @@ public final class Section extends Polygon {
          * \s
         *
         """;
+    }
+
+    @Override
+    public boolean equals(Object anotherObject) {
+        if (anotherObject instanceof Section anotherSection) {
+            return (this.start.equals(anotherSection.getStart())
+                    && this.end.equals(anotherSection.getEnd()))
+                    || (this.start.equals(anotherSection.getEnd())
+                    && this.end.equals(anotherSection.getStart()));
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return this.start.hashCode() + this.end.hashCode();
     }
 }
--- src/test/java/com/chrosciu/geometry/GeometryApplicationTests.java
+++ src/test/java/com/chrosciu/geometry/GeometryApplicationTests.java
@@ -1,5 +1,6 @@
 package com.chrosciu.geometry;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
