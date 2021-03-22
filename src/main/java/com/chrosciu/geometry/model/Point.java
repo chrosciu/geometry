94a4b71dd79daf2fc5101975d06506ba775afef7
Marcin Chrost <marcin.chrost@contractors.roche.com>
2021-03-22 22:34:24 +0100

    stripped version of app for webinar :)

--- src/main/java/com/chrosciu/geometry/model/Point.java
+++
@@ -1,7 +0,0 @@
-package com.chrosciu.geometry.model;
-
-record Point(double x, double y) {
-    double distance(Point p) {
-        return Math.sqrt(Math.pow(p.x - x,2) + Math.pow(p.y - y, 2));
-    }
-}
--- src/main/java/com/chrosciu/geometry/model/Shape.java
+++
@@ -1,7 +0,0 @@
-package com.chrosciu.geometry.model;
-
-public sealed interface Shape permits Circle, Polygon {
-    double getPerimeter();
-    double getArea();
-    Point getGravityCenter();
-}
